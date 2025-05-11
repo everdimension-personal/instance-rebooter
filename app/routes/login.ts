9cc2efdf3f94eda4f72d06af52aa082b134d5651
everdimension <everdimension@gmail.com>
2025-05-11 18:29:17 +0300

    endpoint specs

--- app/routes/login.ts
+++ app/routes/login.ts
@@ -1,4 +1,4 @@
-import { type ActionFunctionArgs } from "@remix-run/node";
+import { type LoaderFunctionArgs, type ActionFunctionArgs } from "@remix-run/node";
 import { getSession, commitSession } from "~/sessions.server";
 import bcrypt from "bcrypt";
 import invariant from "tiny-invariant";
@@ -6,6 +6,23 @@ import { cors } from "~/cors";
 
 invariant(process.env.PASSWORD_HASH_BASE64);
 const PASSWORD_HASH = atob(process.env.PASSWORD_HASH_BASE64);
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const endpoint = `${url.origin}${url.pathname}`;
+  return new Response(
+    `Only POST to this route.
+
+  Request URL: ${endpoint}
+  Request method: POST
+
+  FormData:
+
+    password=your-password
+  `,
+    { status: 405 }
+  );
+}
 
 export async function action({ request }: ActionFunctionArgs) {
   const form = await request.formData();
--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -1,6 +1,22 @@
-import { type ActionFunctionArgs } from "@remix-run/node";
+import {
+  type LoaderFunctionArgs,
+  type ActionFunctionArgs,
+} from "@remix-run/node";
 import { cors } from "~/cors";
 import { destroySession, getSession } from "~/sessions.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const endpoint = `${url.origin}${url.pathname}`;
+  return new Response(
+    `Only POST to this route.
+
+  Request URL: ${endpoint}
+  Request method: POST
+  `,
+    { status: 405 }
+  );
+}
 
 export async function action({ request }: ActionFunctionArgs) {
   const session = await getSession(request.headers.get("Cookie"));

d88afc4cdd2155aa6bf5c5cf8ceb84ba22355809
everdimension <everdimension@gmail.com>
2025-08-10 12:15:28 +0300

    Display and reboot multiple instances

--- app/api/lightsail-instance.ts
+++ app/api/lightsail-instance.ts
@@ -1,41 +1,60 @@
 import {
-  GetInstanceCommand,
+  GetInstancesCommand,
+  Instance,
   LightsailClient,
   RebootInstanceCommand,
 } from "@aws-sdk/client-lightsail";
-import type { ActionFunction } from "react-router";
 import memoizeOne from "memoize-one";
+import type { ActionFunction } from "react-router";
+import { LoaderFunctionArgs } from "react-router";
+import invariant from "tiny-invariant";
 import { cors } from "~/cors";
 import { requireUserSession } from "~/sessions.server";
-import { LoaderFunctionArgs } from "react-router";
 
-const getClient = memoizeOne(
-  () => new LightsailClient({ region: "eu-west-3" })
-);
-export async function getInstanceStats({ request }: LoaderFunctionArgs) {
+const clients = {
+  "eu-west-3": memoizeOne(() => new LightsailClient({ region: "eu-west-3" })),
+  "us-east-1": memoizeOne(() => new LightsailClient({ region: "us-east-1" })),
+};
+
+export async function getInstances({ request }: LoaderFunctionArgs) {
   await requireUserSession(request);
-  const lightsail = getClient();
-  const instanceOutput = await lightsail.send(
-    new GetInstanceCommand({ instanceName: "Ubuntu-1" })
-  );
+  const instances: Instance[] = [];
+  const euWest3 = clients["eu-west-3"]();
+  const usEast1 = clients["us-east-1"]();
+  const [res1, res2] = await Promise.all([
+    euWest3.send(new GetInstancesCommand()),
+    usEast1.send(new GetInstancesCommand()),
+  ]);
+  instances.push(...(res1.instances ?? []));
+  instances.push(...(res2.instances ?? []));
 
-  return {
-    name: instanceOutput.instance?.name ?? null,
-    publicIpAddress: instanceOutput.instance?.publicIpAddress ?? null,
-    state: instanceOutput.instance?.state?.name ?? null,
-    regionName: instanceOutput.instance?.location?.regionName ?? null,
-    createdAt: new Date(instanceOutput.instance?.createdAt || 0),
-  };
+  return instances.map((instance) => ({
+    name: instance?.name ?? null,
+    publicIpAddress: instance?.publicIpAddress ?? null,
+    state: instance?.state?.name ?? null,
+    regionName: instance?.location?.regionName ?? null,
+    createdAt: new Date(instance?.createdAt || 0),
+  }));
 }
 
 export const rebootInstance: ActionFunction = async ({ request }) => {
   await requireUserSession(request);
-  const lightsail = getClient();
-  const res = await lightsail.send(
-    new RebootInstanceCommand({ instanceName: "Ubuntu-1" })
-  );
+  const fd = await request.formData();
+  const regionName = fd.get("regionName")?.toString();
+  const instanceName = fd.get("instanceName")?.toString();
+  if (!regionName || !instanceName) {
+    throw new Error("Invalid params");
+  }
+
+  invariant(regionName in clients, "invalid regionName");
+  console.log("rebooting", { regionName, instanceName });
+  const client = clients[regionName as keyof typeof clients]();
+  const res = await client.send(new RebootInstanceCommand({ instanceName }));
   const response = new Response(JSON.stringify(res));
   return cors(request, response);
 };
 
-export const lightsailInstanceApi = { getInstanceStats, rebootInstance };
+export const lightsailInstanceApi = {
+  rebootInstance,
+  getInstances,
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
+import { HTMLAttributes, useLayoutEffect, useRef } from "react";
 import {
-  type LoaderFunctionArgs,
   redirect,
+  type LoaderFunctionArgs,
   type MetaFunction,
 } from "react-router";
-import { useMutation } from "@tanstack/react-query";
+import invariant from "tiny-invariant";
 import { lightsailInstanceApi } from "~/api/lightsail-instance";
 import type { Route } from "./+types/_index";
 
 export async function loader(params: LoaderFunctionArgs) {
   try {
-    return await lightsailInstanceApi.getInstanceStats(params);
+    return await lightsailInstanceApi.getInstances(params);
   } catch (e) {
     if (e instanceof Response && e.status === 401) {
       return redirect(`/login?next=${encodeURIComponent("/")}`);
     }
   }
+}
+
+function Toast({
+  children,
+  className,
+}: React.PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+  const ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    ref.current?.showPopover();
+  }, []);
+  return (
+    <div
+      ref={ref}
+      popover="auto"
+      className={`bg-white p-2 ${className ?? ""}`}
+      style={{ border: "1px solid CanvasText", marginTop: 20 }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export const meta: MetaFunction = () => {
@@ -26,8 +48,20 @@ export const meta: MetaFunction = () => {
 
 export default function Index({ loaderData }: Route.ComponentProps) {
   const rebootMutation = useMutation({
-    mutationFn: async () => {
-      const res = await fetch("/api/instance/reboot", { method: "POST" });
+    mutationFn: async ({
+      regionName,
+      instanceName,
+    }: {
+      regionName: string;
+      instanceName: string;
+    }) => {
+      const fd = new FormData();
+      fd.append("regionName", regionName);
+      fd.append("instanceName", instanceName);
+      const res = await fetch("/api/instance/reboot", {
+        method: "POST",
+        body: fd,
+      });
       if (res.status >= 400) {
         console.log(res);
         throw new Error(await res.text());
@@ -35,29 +69,68 @@ export default function Index({ loaderData }: Route.ComponentProps) {
       return res;
     },
   });
+  if (!loaderData) {
+    return <p>loading...</p>;
+  }
   return (
-    <div className="flex flex-col items-center gap-8 [padding-top:40px]">
-      <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
-        Lightsail Rebooter
-      </h1>
-      <pre>{JSON.stringify(loaderData, null, 2)}</pre>
-      {rebootMutation.isError ? (
-        <div style={{ textAlign: "center" }}>
-          <small>
-            is error
-            <em className="[color:crimson]">{rebootMutation.error.message}</em>
-          </small>
-        </div>
-      ) : null}
-      <button
-        style={{ color: "var(--link-1)" }}
-        disabled={rebootMutation.isPending}
-        onClick={() => {
-          rebootMutation.mutate();
-        }}
-      >
-        {rebootMutation.isPending ? "Rebooting..." : "Reboot"}
-      </button>
-    </div>
+    <main style={{ maxWidth: 800, marginInline: "auto", paddingInline: 16 }}>
+      <div className="flex flex-col gap-8 [padding-top:40px]">
+        <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
+          Lightsail Instances
+        </h1>
+        {rebootMutation.isError ? (
+          <Toast className="text-red-600">{rebootMutation.error.message}</Toast>
+        ) : null}
+        {rebootMutation.isSuccess ? <Toast>Done</Toast> : null}
+        {loaderData.map((instance) => (
+          <div
+            key={instance.publicIpAddress}
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <div>
+              <b>{instance.name}</b>
+              <div className="text-gray-400 text-sm">
+                {instance.regionName} · {instance.publicIpAddress}
+              </div>
+            </div>
+            <button
+              style={{ color: "var(--link-1)" }}
+              disabled={
+                rebootMutation.isPending &&
+                rebootMutation.variables?.instanceName === instance.name
+              }
+              onClick={() => {
+                invariant(instance.name, "instance must have a name");
+                invariant(
+                  instance.regionName,
+                  "instance must have a regionName",
+                );
+                rebootMutation.mutate({
+                  regionName: instance.regionName,
+                  instanceName: instance.name,
+                });
+              }}
+            >
+              <small>
+                {rebootMutation.isPending &&
+                rebootMutation.variables?.instanceName === instance.name
+                  ? "Rebooting"
+                  : "Reboot"}
+              </small>
+            </button>
+          </div>
+        ))}
+        {rebootMutation.isError ? (
+          <div style={{ textAlign: "center" }}>
+            <small>
+              is error
+              <em className="[color:crimson]">
+                {rebootMutation.error.message}
+              </em>
+            </small>
+          </div>
+        ) : null}
+      </div>
+    </main>
   );
 }
